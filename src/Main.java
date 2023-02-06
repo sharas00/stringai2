70bb47129017754079f512060a0edf16c57df683
sharas00 <sarunas.pusinskis@gmail.com>
2023-02-06 21:48:19 +0200

    junk code cleanup

--- src/Main.java
+++ src/Main.java
@@ -34,5 +34,114 @@ public class Main {
         System.out.println("labas".charAt(2));
         String starWars = "Star Wars: Episode "+ " ".repeat( (int) (Math.random() * 10))+((int) (Math.random() * 7)+1) + " - A New Hope";
         System.out.println(starWars);
+
+
+        System.out.println("----------------------------1--------------------------");
+
+        String fname = "Will";
+        String lname = "Smith";
+
+        if (fname.length() < lname.length()) {
+            System.out.println(fname);
+        }
+            if (fname.length() > lname.length()){
+                System.out.println(lname);
+            }
+
+        System.out.println("----------------------------2--------------------------");
+
+        String firstname = "Johnny";
+        String lastlname = "Depp";
+        System.out.println(firstname.toUpperCase());
+        System.out.println(lastlname.toLowerCase());
+
+        System.out.println("----------------------------3--------------------------");
+
+        String newname = "Bruce";
+        String oldname = "Willis";
+        String initials = newname.charAt(0) + "." + oldname.charAt(0) + ".";
+        System.out.println(initials);
+
+        System.out.println("----------------------------4--------------------------");
+
+        String name1 = "Jim";
+        String lname1 = "Carrey";
+        String last3l = name1.substring (name1.length() - 3) + lname1.substring(lname1.length() - 3);
+        System.out.println(last3l);
+
+        System.out.println("----------------------------5--------------------------");
+
+        String words =  "An American in Paris";
+        System.out.println("An American in Paris".replaceAll("[aA]","*"));
+
+        System.out.println("----------------------------6--------------------------");
+
+        String words2 =  "An American in Paris";
+        String vowelsOut = words2.replaceAll("[aeiouyAEIOUY]","");
+        System.out.println(vowelsOut);
+
+        String breakF = "Breakfast at Tiffany's";
+        String space = "2001: A Space Odyssey";
+        String life = "It's a Wonderful Life";
+        String vowelsOut1 = breakF.replaceAll("[aeiouyAEIOUY]","");
+        String vowelsOut2 = space.replaceAll("[aeiouyAEIOUY]","");
+        String vowelsOut3 = life.replaceAll("[aeiouyAEIOUY]","");
+        System.out.println(vowelsOut1 + " " + vowelsOut2 + " " + vowelsOut3);
+
+        System.out.println("----------------------------7--------------------------");
+
+        String star = "Star Wars: Episode "+ " ".repeat( (int) (Math.random() * 10))+((int) (Math.random() * 7)+1) + " - A New Hope";
+        System.out.println(star);
+
+        String epizNr = star.replaceAll("[^0-9]","");
+        System.out.println("Epizodo numeris yra: " + epizNr);
+
+        System.out.println("----------------------------8--------------------------");
+
+        String juice = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
